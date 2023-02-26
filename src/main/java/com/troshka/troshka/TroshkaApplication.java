c12b66bacf72154e18b7e31ae6ded857710adb09
BapBapuHa <petrov.miroslavov@gmail.com>
2023-02-26 22:56:00 +0200

    initiate spring boot project

---
+++ src/main/java/com/troshka/troshka/TroshkaApplication.java
@@ -0,0 +1,13 @@
+package com.troshka.troshka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroshkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroshkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/troshka/troshka/TroshkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troshka.troshka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroshkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
