6de80f1e4126b6edf38e69074cdc8c52574d70f2
BapBapuHa <petrov.miroslavov@gmail.com>
2023-03-05 23:14:09 +0200

    added UserEntity and UserRoleEntity

---
+++ src/main/java/com/troshka/troshka/entity/UserEntity.java
@@ -0,0 +1,37 @@
+package com.troshka.troshka.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<UserRoleEntity> roles = new LinkedList<>();
+}
---
+++ src/main/java/com/troshka/troshka/entity/UserRoleEntity.java
@@ -0,0 +1,25 @@
+package com.troshka.troshka.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="roles")
+public class UserRoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String name;
+
+    @ManyToMany(mappedBy="roles")
+    private List<UserEntity> users;
+}
